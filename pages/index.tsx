cc5ea53dafa2c3e8d9e5732be9adcc05db201a9c
Ivoine Strachan <70272280+ivoinestrachan@users.noreply.github.com>
2022-12-13 11:42:42 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
 
   let message;
 
-  if (hour >= 0 && hour < 12) {
+  if (hour >= 5 && hour < 12) {
     message = "Im awake at";
   } else if (hour >= 12 && hour < 23) {
     message = "Im coding now at";
