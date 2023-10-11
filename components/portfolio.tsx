879f68f06aa6cf569fea55c8f0ffda8d9b8ebccd
Ivoine <ivoinestrachan242@gmail.com>
2023-10-11 19:29:29 -0400

    got to add more info

---
+++ components/portfolio.tsx
@@ -0,0 +1,120 @@
+import Image from "next/image";
+
+const Portfolio = () => {
+  const journeyData = {
+    journey: [
+      {
+        date: "March 1st 2023",
+        image: [
+          "https://res.cloudinary.com/dxmrcocqb/image/upload/v1697059653/IMG_7261_rbog4t.jpg",
+          "https://res.cloudinary.com/dxmrcocqb/image/upload/v1697059664/IMG_7266_eyprve.jpg",
+          "https://res.cloudinary.com/dxmrcocqb/image/upload/v1697059658/IMG_7259_idd6tr.jpg",
+        ],
+        title: "Flying to San Francisco",
+        desc: "I flew to san francisco with the hope of getting an internship and meeting wonderful new people and building cool project i was able to get a referral at google.",
+      },
+      {
+        date: "Feb 8th 2022",
+        github: "Sinerider",
+        lang: "🟡 Javascript",
+        githubbio: "💖 A game about love and graphing, built by teenagers.",
+        image: [
+          "https://res.cloudinary.com/dxmrcocqb/image/upload/v1697062291/Ivoine_Journey_xqoj1o.png",
+          "https://res.cloudinary.com/dxmrcocqb/image/upload/v1697062288/Ivoine_Journey_1_._dxyzox.png",
+        ],
+        link: "https://github.com/hackclub/sinerider",
+      },
+      {
+        date: "Aug 10th 2023",
+        image: [
+          "https://res.cloudinary.com/dxmrcocqb/image/upload/v1697064339/Sike_Thinking_Image_h9imli.png",
+        ],
+        githubbio: "ios development for a social media app",
+        lang: "🟠 Swift",
+        github: "Bubbel IOS",
+        link: "https://github.com/joinbubbel/bubbel-ios",
+      },
+      {
+        date: "Jan 20th 2023",
+        lang: "🔵 Typescript",
+        github: "microsoft/vscode",
+        githubbio: "Visual Studio Code",
+        link: "https://github.com/microsoft/vscode",
+      },
+      {
+        date: "May 25th 2023",
+        video:
+          "https://res.cloudinary.com/dxmrcocqb/video/upload/v1697065947/4400075460677380078img_8093.MP4_-_Ivoine_Journey_teykpr.mp4",
+        title: "Learnt how to weld",
+        desc: "During my stay in the bay area I learnt how to weld which was pretty cool. I welded a really cool piece together at the end of the day.",
+      },
+      {
+        date: "Oct 1st 2023",
+        lang: "🔵 Typescript",
+        github: "Haunted House",
+        githubbio:
+          "🎃 Where Fright Meets Byte: A Haunted House Hackathon Experience. Oct 28th to 29th 2023 in Downtown Chicago",
+        link: "https://github.com/hackclub/www-hauntedhouse",
+      },
+    ],
+  };
+
+  return (
+    <div className="sm:bg-black sm:w-[280%] w-full sm:h-screen sm:mt-3 rounded-[8px] px-5">
+      <div className="font-bold sm:text-[42px] text-[32px]  pt-10 sm:text-white">
+        Me being creative
+      </div>
+      <div className="text-gray-400 text-[20px] font-medium">
+        Showing my journey and what im working on!
+      </div>
+
+      <div className="sm:overflow-y-auto h-[80vh] mt-5">
+        {journeyData.journey.map((item, index) => (
+          <div key={index}>
+            <p className="text-gray-300 mt-5">{item.date}</p>
+            <div className="flex items-center gap-4 flex-wrap  sm:justify-start justify-center mt-2">
+              {item.image &&
+                //@ts-ignore
+                item.image.map((imageUrl, imgIndex) => (
+                  <div key={imgIndex} className="mx-2">
+                    <Image
+                      src={imageUrl}
+                      alt={`${item.date} Image ${imgIndex + 1}`}
+                      width={350}
+                      height={350}
+                      className="flex items-center rounded-[5px]"
+                    />
+                  </div>
+                ))}
+            </div>
+
+            {item.video && (
+              <div key={`video-${index}`} className="mt-2">
+                <video controls width={400} height={400}>
+                  <source src={item.video} type="video/mp4" />
+                </video>
+              </div>
+            )}
+            <p className="sm:text-white mt-5 font-bold text-[24px]">{item.title}</p>
+            <a href={item.link}>
+              <div
+                className={`border rounded-md pl-3 py-3 sm:w-[25%] ${
+                  item.github ? "border-gray-400" : "border-none"
+                }`}
+              >
+                {item.github && (
+                  <p className="text-blue-500 font-bold">{item.github}</p>
+                )}
+                {item.lang && <p className="sm:text-white">{item.lang}</p>}
+                <p className="sm:text-white">{item.githubbio}</p>
+              </div>
+            </a>
+            <p className="sm:text-white w-[500px]">{item.desc}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,34 +3,21 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
 import Google from "../public/assets/pfp.jpeg";
+import Portfolio from "../components/portfolio";
 
 const Home: NextPage = () => {
-  const [time, setTime] = useState(new Date());
-
-  setInterval(() => setTime(new Date()), 1000);
-
-  const currentTime = new Date();
-
-  const hour = currentTime.getHours();
-
-  let message;
-
-  if (hour >= 5 && hour < 12) {
-    message = "Im awake at";
-  } else if (hour >= 12 && hour < 23) {
-    message = "Im coding now at";
-  } else {
-    message = "Sleeping right now";
-  }
+ 
 
   return (
-    <div className="flex min-h-[700px] flex-col items-center justify-center  max-w-[1024px] md:px-20 m-auto ">
+    <div className="sm:flex">
+      <div>
+    <div className="sm:w-[700px]">
       <Head>
         <title>Ivoine Strachan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="items-center justify-center m-auto w-[100%] md:px-20 px-5">
+      <main className="items-center justify-center m-auto w-[100%] md:px-10 px-5 mt-10">
         <Image
           src={Google}
           alt="ivoine"
@@ -42,16 +29,12 @@ const Home: NextPage = () => {
           Ivoine Strachan
         </div>
 
-        <div className="font-mono font mb-2">
-          {message} {time.toLocaleTimeString()} EST
-        </div>
-
         <div className="flex gap-3 font-mono text-[18px] items-center">
           <a href="https://github.com/ivoinestrachan">
             <p className="underline">Github</p>
           </a>
           <span>~</span>
-          <a href="https://scrapbook.hackclub.com/sikebot">
+          <a href="https://scrapbook.hackclub.com/sike">
             <p className="underline">Scrapbook</p>
           </a>
           <span>~</span>
@@ -59,10 +42,11 @@ const Home: NextPage = () => {
             <p className="underline">Instagram</p>
           </a>
         </div>
+        <div className="font-mono mt-5 text-lg">About Me</div>
 
-        <div className="font-mono mt-5">
+        <div className="font-mono mt-5 sm:hover:bg-gray-100 sm:py-2 sm:pr-3 sm:pl-3 rounded-md cursor-pointer">
           I am a high school dropout with a passion for technology and making a
-          difference. I am currently working on{" "}
+          difference.I am currently working on{" "}
           <span className="underline">
             <a href="https://sinerider.com/">Sinerider</a>
           </span>{" "}
@@ -75,7 +59,25 @@ const Home: NextPage = () => {
           classroom. Join me on this journey and let's build a better future
           together.
         </div>
+
+        <div className="mt-5 font-mono text-lg">
+          <div>
+           Interesting facts about me
+          </div>
+          <div>
+            <li>18 years old</li>
+            <li>I live in The Bahamas</li>
+            <li>built a bomb at a hackathon</li>
+            <li>Travel 2,000 miles to San Francisco for 4months</li>
+            <li>played in a national CARIFTA chess tournament</li>
+          </div>
+        </div>
       </main>
+     </div>
+      </div>
+      <div >
+        <Portfolio />
+      </div>
     </div>
   );
 };
